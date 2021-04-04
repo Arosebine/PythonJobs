9f34d1d60d9ddbb108c5787f1c0ca403b1a35239
Benwaz78 <nonwaz78@gmail.com>
2021-04-04 09:25:00 +0100

    Last class

--- assignment2.py
+++ assignment2.py
@@ -2,7 +2,21 @@
 # display an error message if email is not in the
 # list, if the email is in the list check if the
 # two password fields matches, and if it does, change the
-
-# Assignment 2
 # 1. Create a function that will some all the numbers from a lower limit to an upper limit as seen on number 3 on assignment 2
-# 2. Create a function in Python that will determine average of any numbers in a Python list.
+# 2. Create a function in Python that will determine average of any numbers in a Python list 
+# do this without using any of the inbuilt python function.
+
+
+#3 Create a Class called Product
+#   determine what will be the class varible for the
+#   the product class and create an init method with three
+#   parameters for the product class, create another method
+#   that will calculate the discount prize for the each product
+#   call three times
+
+#4  Create a child class called Shirt
+#  add new paramter that will be a list of sizes in the
+#  init method of the child class. Create a demethod that
+#  display information about this shirt class.
+#  access the attributes on this child class
+
